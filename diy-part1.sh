0aee0049d7e7f199bb9ae701da5c75e6c577b62c
justhil <138752349+justhil@users.noreply.github.com>
2026-03-17 17:34:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# ==================== 性能优先插件 + 主题 + mentohust ====================
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. Passwall + sing-box（科学上网性能王者，轻量 TUN/FakeIP，HNAT 完美兼容）
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
+# 2. Argon 主题（颜值+性能双满分，动画极少）
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+# 3. mentohust（校园网 802.1x）+ luci 前端（sbwml 版，轻量稳定）
+git clone --depth=1 https://github.com/sbwml/luci-app-mentohust package/mentohust
+
+# 其他标准插件无需 clone（feeds 自带）
+# sqm / adblock / upnp / statistics 直接在 .config 开启即可
